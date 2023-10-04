7209903513d110b2374b6dd105e03d3ce5af57b9
eynid <54919045+eynid@users.noreply.github.com>
2023-10-04 15:41:55 -0400

    Update EHou_Project1_Main.java

--- EHou_Project1_Main.java
+++ EHou_Project1_Main.java
@@ -1,3 +1,10 @@
+/*
+*****************
+Project Requirements and Goals: 
+Idea is to obtain the middle node of a given set of numbers inserted in ascending order
+Inputted data is named p1data.txt and p1data2.txt
+*****************
+*/
 import java.io.*;
 import java.util.*;
 
@@ -16,7 +23,6 @@ class LList{
     LList(){}
     
     void constructLL(listNode listHead, Scanner inFile, FileWriter debugFile) throws IOException{
-    //No problems.
         debugFile.write("In construct method \n");
         while(inFile.hasNext()){
             String data = inFile.next();
@@ -25,24 +31,25 @@ class LList{
         }
     }
     void listInsert(listNode listHead, listNode inputData, FileWriter debugFile) throws IOException{
-    //No problems.
         debugFile.write("In listInsert method. \n");
         listNode Spot = findSpot(listHead, inputData);
         debugFile.write("Returning from findSpot, data is: " + Spot.data + "\n");
+        
         inputData.next = Spot.next;
         Spot.next = inputData;
+        
     }
+
+    //Insert data into linked list in ascending order. 
     listNode findSpot(listNode listHead, listNode inputNode){
         listNode Spot = listHead;
         while(Spot.next != null && Spot.next.data.compareTo(inputNode.data) < 0){
             Spot = Spot.next;
         }
-        return Spot;
-        
+        return Spot;  
     }
 
     void printList(listNode listHead, FileWriter outFile) throws IOException{
-        //Works well.
         while(listHead.next != null){
             outFile.write("(" + listHead.data + ", " + listHead.next.data + ") -> ");
             listHead = listHead.next;
@@ -55,7 +62,6 @@ class LList{
     }
 
     listNode findMiddleNode(listNode listHead, FileWriter debugFile) throws IOException{
-        //Works well. 
         debugFile.write("In findMiddleNode method. \n");
         listNode Walker = listHead.next;
         listNode Walker2 = listHead.next;
@@ -64,6 +70,7 @@ class LList{
             Walker = Walker.next;
             Walker2 = Walker2.next.next;
         }
+
         debugFile.write("Walker1's data is: " + Walker.data);
         return Walker;
     }
@@ -74,14 +81,17 @@ public class EHou_Project1_Main{
     Scanner inFile = new Scanner(new FileReader(args[0]));
     FileWriter outFile = new FileWriter(args[1]);
     FileWriter debugFile = new FileWriter(args[2]);
+
     LList linkedList = new LList();
     listNode listHead = new listNode("dummy");
     linkedList.constructLL(listHead, inFile, debugFile);
     linkedList.printList(listHead, outFile);
     listNode middleNode = linkedList.findMiddleNode(listHead, debugFile);
+
     if(middleNode != null){
         outFile.write( "Middle node is: " + middleNode.data);
     }
+
     inFile.close();
     outFile.close();
     debugFile.close();
